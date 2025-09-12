c40c6400712de8d976891596a503b5e7e79051e6
Rick Pfahl <pfahlr@gmail.com>
2025-09-12 01:23:51 -0400

    feat(popup): add svelte entrypoint

---
+++ src/entrypoints/popup/main.ts
@@ -0,0 +1,11 @@
+import App from './App.svelte';
+import './app.css';
+
+const app = new App({
+  target: document.getElementById('app')!,
+});
+
+// Load popup logic after rendering so event handlers attach properly
+import('~/popup');
+
+export default app;
